ca13fd54f8cff1bfa523043a60e0b9e165fe1716
Alton <alton_ong@live.com.sg>
2021-06-30 15:44:18 +1000

    Added themes into app.js

--- design-app/src/App.js
+++ design-app/src/App.js
@@ -1,10 +1,29 @@
-import React from 'react'
-import Home from './pages/Home.js'
+import React from "react";
+import Home from "./pages/Home.js";
+
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			main: "#6C4193",
+			background: "#FFFFFF",
+			content: "#222222"
+		},
+		secondary: "#000",
+		content: "#5C5C5C",
+		disabled_fields: "#ABABAB",
+		background: "#FAFAFA"
+	}
+});
+
 const App = () => {
-  return (
-    <div>
-      <Home/>
-    </div>
-  )
-}
-export default App
+	return (
+		<ThemeProvider theme={theme}>
+			<div>
+				<Home />
+			</div>
+		</ThemeProvider>
+	);
+};
+export default App;
--- design-app/src/pages/Home.js
+++ design-app/src/pages/Home.js
@@ -1,35 +1,41 @@
 // React import
-import React, {useState} from 'react'
+import React, { useState } from "react";
 //Custom components
-import CustomButton from '../commons/CustomButton'
-import {Button} from "@material-ui/core"
+import CustomButton from "../commons/CustomButton";
+import { Button } from "@material-ui/core";
 
 //Custom styling import
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const CustomDiv = styled.div`
-margin: 100px;
-`
-
+	margin: 100px;
+`;
 
 const Home = () => {
-
-    const [background, setBackground] = useState('white');
-    const buttonClicked = () => {
-        setBackground('secondary')
-        console.log(background)
-    }
-    return (
-        <div>
-            <h1>
-                Home
-            </h1>
-            <Button onClick={(e) => buttonClicked(e)}>Press me</Button>
-            <CustomDiv>
-                <CustomButton color={background} buttonClicked={(e)=> buttonClicked(e)} variant="outlined" text="Default" />
-            </CustomDiv>
-            <CustomButton variant = "contained" text = "Hoiiii" />
-        </div>
-    )
-}
-export default Home
+	const [background, setBackground] = useState("white");
+	const buttonClicked = (e) => {
+		setBackground("secondary");
+		console.log(background);
+	};
+	return (
+		<div>
+			<h1>Home</h1>
+			<Button
+				variant="contained"
+				color="primary"
+				onClick={(e) => buttonClicked(e)}
+			>
+				Press me
+			</Button>
+			<CustomDiv>
+				<CustomButton
+					color={background}
+					buttonClicked={(e) => buttonClicked(e)}
+					variant="outlined"
+					text="Default"
+				/>
+			</CustomDiv>
+		</div>
+	);
+};
+export default Home;
